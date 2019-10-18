c22cf219fa4586b2cbbee7ab3fcf2e4deb517325
shiggy730 <shiggy730@gmail.com>
2019-10-17 21:23:15 -0700

    Interrupt based wakeup

--- dark_motion_light.ino
+++ dark_motion_light.ino
@@ -13,8 +13,8 @@
 #define timeMotionSeconds 10
 #define timeLightSensSeconds 3
 #define lightIgnoreVal 4095
-#define darkVal 20
-#define sleepWhenLight 30000000
+#define darkVal 10
+#define sleepAtWakeup 2
 #define reducedClockSpeed 80
 
 // Set GPIOs for photo resister, LED, PIR Motion Sensor
@@ -23,26 +23,13 @@ const int led = 33;
 const int motionSensor = 15;
 
 // Motion timer variables
-unsigned long now = millis();
-unsigned long lastMotionTrigger = 0;
-boolean startMotionTimer = false;
+unsigned long now;
 
 // Photo resistor variables
 int curLightVal;
 int lightVal;
 int lightValCount;
 unsigned long lastLightTime;
-boolean isDark = false;
-
-// Checks if motion was detected, sets LED HIGH and starts a timer
-void IRAM_ATTR detectsMovement() {
-  //Serial.println("MOTION DETECTED!!!");
-  if (isDark) {
-    digitalWrite(led, HIGH);
-  }
-  startMotionTimer = true;
-  lastMotionTrigger = millis();
-}
 
 void setup() {
 
@@ -60,58 +47,53 @@ void setup() {
   
   // PIR Motion Sensor mode INPUT_PULLUP
   pinMode(motionSensor, INPUT_PULLUP);
-  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
-
+  
   // Set LED to LOW
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
 
-  // set darkness sensing variables
-  lastLightTime = millis();
+  // set darkness sensing variables  
   lightVal = 0;
   lightValCount = 0;
 
-  esp_sleep_enable_timer_wakeup(sleepWhenLight);  
+  gpio_wakeup_enable(GPIO_NUM_15, GPIO_INTR_HIGH_LEVEL);
+  esp_sleep_enable_gpio_wakeup();
 }
 
 void loop() {
   
-  // Current time
-  delay(2000);
-  now = millis();
+  // Wait a bit after wakeup
+  delay(sleepAtWakeup);  
 
   // Capture photo resistor reading.
   // Ignore lightIgnoreVal, which happens sporadically whether dark or light.
   // Get the total during the reading period so it can be averaged
-  curLightVal = analogRead(photoResistor);
-  if (curLightVal != lightIgnoreVal) {
-    lightVal += analogRead(photoResistor);
-    lightValCount += 1;
-  }
-
-  // Take the average reading for specified time to determine if it is dark.
-  if (now - lastLightTime > (timeLightSensSeconds*1000)) {
-    lightVal = lightVal / lightValCount;
-    //Serial.println("Light sensor read: ");
-    //Serial.println(lightVal);
-    if (lightVal < darkVal) {
-      isDark = true;
-      //Serial.println("It is dark...");
-    } else {
-      isDark = false;
-      //Serial.println("It is light...");
-      esp_deep_sleep_start();
+  now = millis();
+  lightVal = 0;
+  lightValCount = 0;
+  lastLightTime = millis();
+  while (lastLightTime - now < (timeLightSensSeconds*1000)) {
+    curLightVal = analogRead(photoResistor);
+    if (curLightVal != lightIgnoreVal) {
+      lightVal += analogRead(photoResistor);
+      lightValCount += 1;
     }
-    lightVal = 0;
-    lightValCount = 0;
     lastLightTime = millis();
   }
-    
-  // Turn off the LED after defined time.
-  if(startMotionTimer && (now - lastMotionTrigger > (timeMotionSeconds*1000))) {
-    //Serial.println("Turn off LED...");
+
+  // Take the average reading for specified time to determine if it is dark.  
+  lightVal = lightVal / lightValCount;
+  //Serial.println("Light sensor read: ");
+  //Serial.println(lightVal);
+  if (lightVal < darkVal) {
+    //Serial.println("It is dark...");
+    digitalWrite(led, HIGH);
+    delay(timeMotionSeconds*1000);
     digitalWrite(led, LOW);
-    startMotionTimer = false;
+  } else {      
+    //Serial.println("It is light..."); 
   }
+    
+  // enter deep sleep  
+  esp_light_sleep_start();
 }
